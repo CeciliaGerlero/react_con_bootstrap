92454c3f336d22ef7a98fa2734496679d9cf67ba
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-22 09:17:03 -0300

    ajustes clase 27

--- src/components/Pagina.js
+++ src/components/Pagina.js
@@ -37,20 +37,31 @@ function Pagina() {
         </Container>
       </Navbar>
 
+
+      
+
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
+          {/* <ExampleCarouselImage text="First slide" /> */}
 
           <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img01} alt="First slide" />
-          
+          {/* <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img02} alt="First slide" />
+          <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img03} alt="First slide" /> */}
+
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
-
-          
-
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* <ExampleCarouselImage text="Second slide" /> */}
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* <ExampleCarouselImage text="Third slide" /> */}
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
@@ -59,6 +70,7 @@ function Pagina() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+
     </>
   );
 
